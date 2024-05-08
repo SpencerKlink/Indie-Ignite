337feb2a578f16ad2f6a8aa55b5be69ed15395fc
Christopher Cuellar <rival100x@gmail.com>
2024-05-07 20:30:41 -0400

    added login on the controller and created models

--- models/Review.js
+++ models/Review.js
@@ -1,1 +1,41 @@
-// Reviews model
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Review extends Model {}
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        review_text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        game_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'game',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review'
+    }
+);
+
+module.exports = Review;
