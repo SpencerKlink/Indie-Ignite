9d4279519f22800fe8ca073b8e063ef5ea31365c
Spencer Klinkowize <spencerk@Spencers-MacBook-Pro-M3.local>
2024-05-09 11:39:11 -0400

    created structure of users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,1 +1,74 @@
-//  Routes for user-related operations
+const express = require('express');
+const router = express.Router();
+const { User } = require('../../models'); 
+const bcrypt = require('bcrypt');
+
+// POST route for user registration (signup)
+router.post('/signup', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, 10)
+        });
+
+        // Setting up user session upon signup
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            req.session.username = newUser.username;
+            req.session.loggedIn = true;
+
+            res.status(201).json(newUser);
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+// POST route for user login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'No user found with that email address!' });
+            return;
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.username = user.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: user, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+// POST route for user logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
