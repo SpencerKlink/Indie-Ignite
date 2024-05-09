304a553de0fca5e81aaca6a0f7e60e2b78a9eb7e
Michael Freeman <mikef@Michaels-MBP.lan>
2024-05-09 19:01:36 -0400

    added redirect to login if not logged in functionality, and added wildcard route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const withAuth = require('../config/middleware/auth')
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     res.render('home'), { layout: false };  
 });
 
 router.get('/login', (req, res) => {
     res.render('login', { layout: false });  
+});
+
+router.get('*', withAuth, (req, res) => {
+    res.render('home'), { layout: false };  
 });
 
 module.exports = router;
