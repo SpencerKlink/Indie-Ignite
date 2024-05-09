93b7696ee1a3c5b8bfc936db59a38e5201f1a720
Spencer Klinkowize <spencerk@Spencers-MacBook-Pro-M3.local>
2024-05-09 12:26:34 -0400

    removed the gameController.js file and split the purpose of the adminGameRoutes.js and the pulicGameRoutes.js file to take over that to make it more clear

---
+++ routes/api/publicGameRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { Game } = require('../models');
+
+// GET route to retrieve all games
+router.get('/', async (req, res) => {
+    try {
+        const games = await Game.findAll();
+        res.json(games);
+    } catch (error) {
+        console.error('Error getting games:', error);
+        res.status(500).json({ message: 'Failed to get games', error });
+    }
+});
+
+// GET route to retrieve a single game by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const game = await Game.findByPk(req.params.id);
+        if (!game) {
+            res.status(404).json({ message: 'Game not found' });
+        } else {
+            res.json(game);
+        }
+    } catch (error) {
+        console.error('Error finding game:', error);
+        res.status(500).json({ message: 'Failed to find game', error });
+    }
+});
+
+module.exports = router;
