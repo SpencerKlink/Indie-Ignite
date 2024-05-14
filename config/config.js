aaaf70afffb9ac8204cef2b5d717955f765ac706
Spencer Klinkowize <spencerk@Spencers-MacBook-Pro-M3.local>
2024-05-13 20:58:57 -0400

    another update to the config for JAWSDB

--- config/config.js
+++ config/config.js
@@ -1,48 +1,50 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const env = process.env.NODE_ENV || 'development'; 
-const config = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        port: 3306 
-    },
-    test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        port: 3306 
-    },
-    production: {
-        username: process.env.DB_USER || "root",
-        password: process.env.DB_PASS, 
-        database: process.env.DB_NAME || "database_production",
-        host: process.env.DB_HOST || "localhost",
-        dialect: 'mysql',
-        port: 3306 
-    }
-}[env];
+// const env = process.env.NODE_ENV || 'development'; 
+// const config = {
+//     development: {
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: process.env.DB_NAME,
+//         host: process.env.DB_HOST,
+//         dialect: 'mysql',
+//         port: 3306 
+//     },
+//     test: {
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//         database: process.env.DB_NAME,
+//         host: process.env.DB_HOST,
+//         dialect: 'mysql',
+//         port: 3306 
+//     },
+//     production: {
+//         username: process.env.DB_USER || "root",
+//         password: process.env.DB_PASS, 
+//         database: process.env.DB_NAME || "database_production",
+//         host: process.env.DB_HOST || "localhost",
+//         dialect: 'mysql',
+//         port: 3306 
+//     }
+// }[env];
 
-let sequelize; 
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {
-        dialect: 'mysql',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false 
-            }
-        }
-    });
+ console.log(process.env.JAWSDB_URL)
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
 }
 
 sequelize.authenticate()
