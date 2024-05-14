1778032d84ee09351b6dc80684670447c9e0a2a7
Spencer Klinkowize <spencerk@Spencers-MacBook-Pro-M3.local>
2024-05-14 15:39:25 -0400

    created edit profile page layout and removed elements from the original proflie page

--- routes/api/profileApi.js
+++ routes/api/profileApi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
+const isAuthenticated = require('../../config/middleware/auth'); 
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
@@ -22,5 +23,20 @@ router.post('/upload-photo', upload.single('profileImage'), (req, res) => {
         .then(() => res.redirect('/profile'))
         .catch(error => res.status(500).send('Error updating profile image: ' + error.message));
 });
+
+router.post('/update/:userId', isAuthenticated, (req, res) => {
+    const User = require('../../models/User'); 
+    if (req.params.userId !== req.user.id.toString()) {
+        return res.status(403).send('Unauthorized');
+    }
+    User.updateOne({_id: req.params.userId}, {$set: req.body}, function(err, result) {
+        if (err) {
+            return res.status(500).send('Error updating profile: ' + err);
+        }
+        res.redirect(`/profile/${req.params.userId}`);
+    });
+});
+
+module.exports = router;
 
 module.exports = router;
