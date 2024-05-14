660c80a007bcb3b1d3ca7eb9c5a451fe12194d9f
Christopher Cuellar <rival100x@gmail.com>
2024-05-14 18:54:40 -0400

    added routes for rendering database content

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,32 +1,52 @@
 const express = require('express');
 const router = express.Router();
-// const isAuth = require('../config/middleware/auth')
+const { Game, User } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('home'), { layout: false };  
+router.get('/', async (req, res) => {
+    try {
+        const gameData = await Game.findAll();
+
+        const games = gameData.map((game) => game.get({ plain: true }));
+
+        res.render('home', { games, layout: false });
+    } catch (error) {
+        console.error('Error getting games:', error);
+        res.status(500).json({ message: 'Failed to get games', error });
+    }
 });
 
 router.get('/login', (req, res) => {
-    res.render('login', { layout: false });  
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login', { layout: false });
 });
 
-router.get('/profile', (req, res) => {
-    res.render('profile'), { layout: false };  
+router.get('/profile', async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            id: req.session.userId
+        },
+        include: [Game]
+    });
+    res.render('profile', { user, layout: false });
 });
-
 
 router.get('/game', (req, res) => {
-    res.render('gamePage'), { layout: false };  
+    res.render('gamePage', { layout: false });  
 });
 
-
-
-
-
-// router.get('*', (req, res) => {
-//     res.render('home'), { layout: false };  
-// });
-
-
+// GET route to retrieve a specific game by its ID and render the gamecard partial
+router.get('/game/:id', async (req, res) => {
+    try {
+        const game = await Game.findByPk(req.params.id);
+        res.render('gamecard', game);
+    } catch (error) {
+        console.error('Error getting game:', error);
+        res.status(500).json({ message: 'Failed to get game', error });
+    }
+});
 
 module.exports = router;
