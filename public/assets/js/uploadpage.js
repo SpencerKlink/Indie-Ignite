aaf315d2b0d73487fe751c9a838f9fb70b0b48de
Anthony Santos <anthony.santos1824@gmail.com>
2024-05-16 19:28:52 -0400

    stacked the containers when the screensize gets smaller

---
+++ public/assets/js/uploadpage.js
@@ -0,0 +1,22 @@
+function checkWidth() {
+    const dropZone = document.getElementById('drop_zone');
+    const mainContentContainer = document.getElementById('main-content-container');
+    const supporterPackages = document.getElementById('supporter-packages');
+
+    if (window.innerWidth < 992) {
+        dropZone.classList.add('hidden');
+    } else {
+        dropZone.classList.remove('hidden');
+    }
+
+    if (window.innerWidth > 768) {
+        mainContentContainer.appendChild(supporterPackages);
+    } else {
+        document.querySelector('.container').appendChild(supporterPackages);
+    }
+}
+
+window.addEventListener('resize', checkWidth);
+window.addEventListener('load', checkWidth);
+
+
